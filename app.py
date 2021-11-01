a6b51e428755e633bdebbb61b59fd645200d8f6b
Sungeheon <elon4856@gmail.com>
2021-11-01 19:50:25 +0900

    feat:blueprint를 적용.

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def create_app():
+    app = Flask(__name__)
+
+    from views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
