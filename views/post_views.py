e0f1480a56e9f6710e3cf0a5ab24f7838da00980
Sungeheon <elon4856@gmail.com>
2021-11-02 00:10:28 +0900

    feat: 게시판 crud api기능 구현.

--- views/post_views.py
+++ views/post_views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, jsonify
 import json
 from flask import request, jsonify
-from models.models import User
+from models.models import User, Post
 
 bp = Blueprint('post', __name__, url_prefix='/')
 
@@ -11,11 +11,30 @@ def hello_world():
     return 'Hello! blueprint'
 
 
-# @doc(tags=['post'], description='게시글을 저장한다.')
 @bp.route('/create', methods=['POST'])
-@use_kwargs(CreatePostRequestSchema)
-@marshal_with(None, code=201)
-def create(subject, content):
-    post_service.create_post(subject, content, current_user)
-    return make_response(jsonify(msg='success', status_code=201), 201)
+def create():
+    body = request.get_json()
+    post = Post(**body).save()
+    id = post.id
+    return make_response(jsonify(msg='success', status_code=201, id=str(id)), 201)
 
+
+@bp.route('/posts/<id>', methods=['GET'])
+def read(id):
+    post = Post.objects.get_or_404(id=id)
+    return make_response(jsonify(post))
+
+
+@bp.route('/posts/<id>', methods=['PUT', 'PATCH'])
+def update(id):
+    body = request.get_json()
+    post = Post.objects.get_or_404(id=id)
+    post.update(**body)
+    return make_response(jsonify(msg='success', status_code=201, id=str(id)), 201)
+
+
+@bp.route('/posts/<id>', methods=['DELETE'])
+def delete(id):
+    post = Post.objects.get_or_404(id=id)
+    post.delete()
+    return make_response('', 204)
