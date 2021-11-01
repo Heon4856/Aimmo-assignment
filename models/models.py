b63537e59150c2bc780a61d3f9fb847fcdca35c5
Sungeheon <elon4856@gmail.com>
2021-11-01 23:17:17 +0900

    feat:mongodb 설치 및 적용.

---
+++ models/models.py
@@ -0,0 +1,10 @@
+from db import db
+
+
+class User(db.Document):
+    name = db.StringField()
+    email = db.StringField()
+
+    def to_json(self):
+        return {"name": self.name,
+                "email": self.email}
