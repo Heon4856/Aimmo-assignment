4a535b4b0262db02a1cf0bc3d8612388c7975f3e
Daehoon <foreat_14@naver.com>
2021-11-02 15:13:54 +0900

    feat : 조회수 증가 로직 생성

--- repository/post_repository.py
+++ repository/post_repository.py
@@ -6,16 +6,21 @@ def read_post_detail(id):
 
 
 def create(title, content, date, current_user):
-    post = Post(title=title, content=content, create_date=date, user=current_user).save()
+    post = Post(title=title, content=content, create_date=date, user=current_user, hits=0).save()
     id = post.id
     return id
 
 
 def modify(id, title, content, modify_date, current_user):
     post = Post.objects.get_or_404(id=id)
-    post.update(title=title, content=content, modify_date=modify_date, user=current_user)
+    post.update(title=title, content=content, modify_date=modify_date, user=current_user, hits=post.hits)
 
 
 def delete(id, current_user):
     post = Post.objects.get_or_404(id=id)
     post.delete()
+
+
+def hit(id):
+    post = Post.objects.get_or_404(id=id)
+    post.update(hits=post.hits+1)
